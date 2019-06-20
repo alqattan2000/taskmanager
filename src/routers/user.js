9f61d57d22e3f1cf80a6f139dbbd1cbcfeb2e0cb
alqattan2000 <alqattan2000@gmail.com>
2019-06-20 08:45:13 +0300

    mlab

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const validator = require('validator')
 
 /* Users APIs */
 // Add user
